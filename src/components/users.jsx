ae31250ebf9f0f8d0741a0a8b870b8f89782e493
goodAssistant <eestistone@gmail.com>
2023-01-31 21:42:03 +0300

    Сourse_Project:_Task#1

---
+++ src/components/users.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import api from '../api'
+
+const Users = () => {
+
+ const [users, setUsers] = useState(api.users.fetchAll())
+
+ const handleDelete = userId => {
+  setUsers(users => users.filter(el => el._id !== userId))
+ }
+
+ const renderPhrase = number => {
+  let lastEl = String(number).at(-1)
+  if(number) {
+   if(number > 20 && lastEl == 2 || lastEl == 3 || lastEl == 4) {
+    number += ' человека'
+   }else if(number === 2 || number === 3 || number === 4) {
+    number += ' человека'
+   }else {
+    number += ' человек'
+   }
+   return number + ' тусанёт с тобой сегодня'
+  } else return 'Никто с тобой не тусанёт'
+ }
+
+ return (
+ <>
+  <h4 className={'fs-4 badge m-2 ' + (users.length ? 'bg-primary' : 'bg-danger')}>{renderPhrase(users.length)}</h4>
+  {users.length ? <table className="table">
+   <thead>
+    <tr>
+     <th scope="col">Имя</th>
+     <th scope="col">Качества</th>
+     <th scope="col">Профессия</th>
+     <th scope="col">Встретился, раз</th>
+     <th scope="col">Оценка</th>
+     <th></th>
+    </tr>
+   </thead>
+   <tbody>
+    {users.map(row => 
+     <tr key={row._id}>
+      <td key={row.name}>{row.name}</td>
+      <td>
+       {row.qualities.map(elem => 
+       <span className={'badge bg-' + elem.color + ' m-1'} key={elem._id}>
+        {elem.name}
+       </span>)}
+      </td>
+      <td key={Object.values(row.profession)[0]}>{Object.values(row.profession)[1]}</td>
+      <td key={row.completedMeetings}>{row.completedMeetings}</td>
+      <td key={row.rate}>{row.rate}/5</td>
+      <td>
+       <button className="btn btn-danger" onClick={() => handleDelete(row._id)}>delete</button>
+      </td>
+     </tr>
+     )}
+   </tbody>
+  </table> : null}
+  </>
+ )
+}
+
+export default Users
